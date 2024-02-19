a64981c8e5234cdfa1d73c8c4a35f47480bc2840
azabarnahaju <kanikalexandra@gmail.com>
2024-02-19 04:45:02 +0100

    Add CityDataProvider interface and implementation

---
+++ SolarWatch/Services/CityDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+
+namespace SolarWatch.Services;
+
+public class CityDataProvider : ICityDataProvider
+{
+    private readonly ILogger<CityDataProvider> _logger;
+
+    public CityDataProvider(ILogger<CityDataProvider> logger)
+    {
+        _logger = logger;
+    }
+
+    public string GetCity(string cityName)
+    {
+        var apiKey = "d80b2959da1f5d7225828323dee566bd";
+        
+        var url = $"http://api.openweathermap.org/geo/1.0/direct?q={cityName}&limit=1&appid={apiKey}";
+
+        using var client = new WebClient();
+        
+        _logger.LogInformation("Calling OpenWeather API with url: {}", url);
+        return client.DownloadString(url);
+    }
+}
---
+++ SolarWatch/Services/ICityDataProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace SolarWatch.Services;
+
+public interface ICityDataProvider
+{
+    string GetCity(string cityName);
+}
