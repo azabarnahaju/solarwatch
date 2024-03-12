4a2e6023555c98ab605a1a536ab30eec36cd5953
azabarnahaju <kanikalexandra@gmail.com>
2024-03-12 18:41:32 +0100

    Add UsersContext

---
+++ SolarWatch/Data/UsersContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SolarWatch.Data;
+
+public class UsersContext : IdentityUserContext<IdentityUser>
+{
+    public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost,1433;Database=SolarWatch;User Id=sa;Password=DaBaPaWo2024;Encrypt=false;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
