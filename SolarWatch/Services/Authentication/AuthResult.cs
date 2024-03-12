1c9b4e88a5b8bc58420f683c22c11698b25360fa
azabarnahaju <kanikalexandra@gmail.com>
2024-03-12 18:43:47 +0100

    Add AuthService and AuthController

---
+++ SolarWatch/Services/Authentication/AuthResult.cs
@@ -0,0 +1,6 @@
+﻿namespace SolarWatch.Services.Authentication;
+
+public record AuthResult(bool Success, string Email, string UserName, string Token)
+{
+    public readonly Dictionary<string, string> ErrorMessages = new ();
+}
