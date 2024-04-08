e4d201165acd4ad77130dfdaa8dc0f393f368fbf
azabarnahaju <kanikalexandra@gmail.com>
2024-04-08 21:04:59 +0200

    Fix WebApplicationFactory services and basic tests, add test data seeding

--- SolarWatch.IntegrationTests/IntegrationTests/BasicTests.cs
+++ SolarWatch.IntegrationTests/IntegrationTests/BasicTests.cs
@@ -14,8 +14,7 @@ public class BasicTests : IClassFixture<SolarWatchWebApplicationFactory<Program>
     private readonly SolarWatchWebApplicationFactory<Program> _factory;
     private readonly HttpClient _client;
     
-    public BasicTests(
-        SolarWatchWebApplicationFactory<Program> factory)
+    public BasicTests(SolarWatchWebApplicationFactory<Program> factory)
     {
         _factory = factory;
         _client = _factory.CreateClient();
@@ -24,17 +23,23 @@ public class BasicTests : IClassFixture<SolarWatchWebApplicationFactory<Program>
     [Fact]
     public async Task Should_Reject_Unauthenticated_Requests()
     {
-        var response = await _client.GetAsync("/Sunrise/GetSunrise");
+        var response = await _client.GetAsync("/Sunrise/GetSunrise?cityName=Budapest");
         response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
     }
     
     [Theory]
-    [InlineData("/Sunrise/GetSunrise?cityName=TestCity")]
-    [InlineData("/Sunrise/GetSunriseOnDate?cityName=TestCity&date=2023-05-06")]
-    [InlineData("/Sunrise/GetSunriseOnDate?cityName=TestCity")]
-    [InlineData("/Sunset/GetSunset?cityName=TestCity")]
-    [InlineData("/Sunset/GetSunsetOnDate?cityName=TestCity&date=2023-05-06")]
-    [InlineData("/Sunset/GetSunsetOnDate?cityName=TestCity")]
+    [InlineData("/Sunrise/GetSunrise?cityName=Paris")]
+    [InlineData("/Sunrise/GetSunriseOnDate?cityName=Paris&date=2023-04-08")]
+    [InlineData("/Sunrise/GetSunriseOnDate?cityName=Paris")]
+    [InlineData("/Sunset/GetSunset?cityName=Paris")]
+    [InlineData("/Sunset/GetSunsetOnDate?cityName=Paris&date=2023-04-08")]
+    [InlineData("/Sunset/GetSunsetOnDate?cityName=Paris")]
+    [InlineData("/Sunrise/GetSunrise?cityName=Budapest")]
+    [InlineData("/Sunrise/GetSunriseOnDate?cityName=Budapest&date=2023-04-06")]
+    [InlineData("/Sunrise/GetSunriseOnDate?cityName=Budapest")]
+    [InlineData("/Sunset/GetSunset?cityName=Budapest")]
+    [InlineData("/Sunset/GetSunsetOnDate?cityName=Budapest&date=2023-04-06")]
+    [InlineData("/Sunset/GetSunsetOnDate?cityName=Budapest")]
     public async Task Should_Allow_All_RegisteredUsers_Get(string url)
     {
         var token = new TestJwtToken().WithRole("User").WithName("testuser").Build();
@@ -45,19 +50,29 @@ public class BasicTests : IClassFixture<SolarWatchWebApplicationFactory<Program>
     }
     
     [Fact]
-    public async Task Should_Allow_Admin_Update_Or_Add_City()
+    public async Task Should_Allow_Admin_Update_City()
     {
         var token = new TestJwtToken().WithRole("Admin").WithName("Admin").Build();
         _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-        var city = new City { Id = 1, Country = "TEST", Lat = 1.23456, Lon = 1.23456, Name = "TestCity", State = "TestState"};
+        var city = new City { Id = 1, Country = "HU", Lat = 47.4979937, Lon = 19.0403594, Name = "Budapest", State = "Pest"};
         var content = new StringContent(JsonConvert.SerializeObject(city), Encoding.UTF8, "application/json");
         
-        var responseUpdate = await _client.PatchAsync("/City/Update", content);
-        var responseAdd = await _client.PostAsync("/City/Add", content);
+        var response = await _client.PatchAsync("/City/Update", content);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    [Fact]
+    public async Task Should_Allow_Admin_Add_City()
+    {
+        var token = new TestJwtToken().WithRole("Admin").WithName("Admin").Build();
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
         
-        responseUpdate.StatusCode.Should().Be(HttpStatusCode.OK);
-        responseAdd.StatusCode.Should().Be(HttpStatusCode.OK);
+        var city = new City { Country = "TEST", Lat = 1.23456, Lon = 1.23456, Name = "Test", State = "Test" };
+        var content = new StringContent(JsonConvert.SerializeObject(city), Encoding.UTF8, "application/json");
+        
+        var response = await _client.PostAsync("/City/Add", content);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
     
     [Fact]
@@ -77,19 +92,29 @@ public class BasicTests : IClassFixture<SolarWatchWebApplicationFactory<Program>
     }
     
     [Fact]
-    public async Task Should_Allow_Admin_Update_Or_Add_Sunrise()
+    public async Task Should_Allow_Admin_Update_Sunrise()
     {
         var token = new TestJwtToken().WithRole("Admin").WithName("Admin").Build();
         _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-        var city = new Sunrise { Id = 1, CityId = 1, Date = new DateTime(2024, 5, 5), Time = ""};
+        var city = new Sunrise { Id = 1, CityId = 1, Date = new DateTime(2024, 4, 6), Time = "4:09 AM"};
         var content = new StringContent(JsonConvert.SerializeObject(city), Encoding.UTF8, "application/json");
         
-        var responseUpdate = await _client.PatchAsync("/Sunrise/Update", content);
-        var responseAdd = await _client.PostAsync("/Sunrise/Add", content);
+        var response = await _client.PatchAsync("/Sunrise/Update", content);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    [Fact]
+    public async Task Should_Allow_Admin_Add_Sunrise()
+    {
+        var token = new TestJwtToken().WithRole("Admin").WithName("Admin").Build();
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var city = new Sunrise { CityId = 1, Date = new DateTime(2024, 4, 8), Time = "TEST2"};
+        var content = new StringContent(JsonConvert.SerializeObject(city), Encoding.UTF8, "application/json");
         
-        responseUpdate.StatusCode.Should().Be(HttpStatusCode.OK);
-        responseAdd.StatusCode.Should().Be(HttpStatusCode.OK);
+        var response = await _client.PostAsync("/Sunrise/Add", content);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
     
     [Fact]
@@ -109,19 +134,29 @@ public class BasicTests : IClassFixture<SolarWatchWebApplicationFactory<Program>
     }
     
     [Fact]
-    public async Task Should_Allow_Admin_Update_Or_Add_Sunset()
+    public async Task Should_Allow_Admin_Update_Sunset()
     {
         var token = new TestJwtToken().WithRole("Admin").WithName("Admin").Build();
         _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-        var city = new Sunset { Id = 1, CityId = 1, Date = new DateTime(2024, 5, 5), Time = ""};
+        var city = new Sunset { Id = 1, CityId = 1, Date = new DateTime(2024, 4, 6), Time = "5:22 PM"};
         var content = new StringContent(JsonConvert.SerializeObject(city), Encoding.UTF8, "application/json");
         
-        var responseUpdate = await _client.PatchAsync("/Sunset/Update", content);
-        var responseAdd = await _client.PostAsync("/Sunset/Add", content);
+        var response = await _client.PatchAsync("/Sunset/Update", content);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    [Fact]
+    public async Task Should_Allow_Admin_Add_Sunset()
+    {
+        var token = new TestJwtToken().WithRole("Admin").WithName("Admin").Build();
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var city = new Sunset { CityId = 1, Date = new DateTime(2024, 4, 8), Time = "TEST"};
+        var content = new StringContent(JsonConvert.SerializeObject(city), Encoding.UTF8, "application/json");
         
-        responseUpdate.StatusCode.Should().Be(HttpStatusCode.OK);
-        responseAdd.StatusCode.Should().Be(HttpStatusCode.OK);
+        var response = await _client.PostAsync("/Sunset/Add", content);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
     }
     
     [Fact]
